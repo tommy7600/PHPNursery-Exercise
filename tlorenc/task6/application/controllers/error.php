3facc4102fcf2d7f2036a274a7cd483b946e8a9f
Tomasz Lorenc <tlorenc@future-processing.com>
2012-12-10 10:23:31 +0100

    Task 6: Add download section

--- tlorenc/task6/application/controllers/error.php
+++ tlorenc/task6/application/controllers/error.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class error implements IController
 {
     const VIEWS_FOLDER = '../views/error/';
--- tlorenc/task6/application/views/download/index.php
+++ tlorenc/task6/application/views/download/index.php
@@ -7,7 +7,23 @@ $menu = '<ul class="nav">
                     <li><a href="contact">Contact</a></li>
                 </ul>';
 
-$content = $this->files;
+$content = '<div class="row">
+    <table width="500" class="table table-striped table-hover">
+        <thead>
+        <tr>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Download</th>
+        </tr>
+        </thead>
+        <tbody>';
+
+
+$content .= $this->files;
+
+$content .= ' </tbody>
+    </table>
+</div>';
 
 $template = new Template('Task 6 - GALAXY', 'res/templates/html/index.html.php');
 $template->setContent('{MENU}', $menu);
